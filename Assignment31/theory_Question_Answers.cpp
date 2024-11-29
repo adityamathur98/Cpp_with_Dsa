b713831b2d1ce79c7e949c2aeb5f2b1c15a5b675
aditya <adityamathur1889@gmail.com>
2024-11-29 22:16:27 +0530

    Submit Assignment File on map, set, struct datastructure

---
+++ Assignment31/theory_Question_Answers.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    /*
+        Compare the following data structures: map, vector, set, and array.
+            Which one maintains order?
+            Which one allows duplicates?
+            Which one has faster random access?
+            When would you choose one over the other in a real-world scenario?
+    */
+
+    // Answer
+    /*
+        Summary Table
+        Feature	            | map	             vector	                set	                    array
+        Maintains Order	    | Sorted (by key)	| Insertion order	| Sorted (unique)	| Insertion order
+        Allows Duplicates	| No (multimap)	    | Yes	            | No (multiset)	    | Yes
+        Random Access	    | No	            | Yes (O(1))	    | No	            | Yes (O(1))
+        Real-World Use Case	| Key-value pairs	| Dynamic list	    | Unique items	    | Fixed-size list
+    */
+    return 0;
+}
